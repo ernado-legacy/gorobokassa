d348e89249f577ec60bce8dd512bcf412ae45d7c
ernado <ernado@ya.ru>
2014-06-23 21:45:20 +0400

    init

---
+++ gorobokassa.go
@@ -0,0 +1,99 @@
+package gorobokassa
+
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+const (
+	QUERY_OUT_SUMM    = "OutSum"
+	QUERY_INV_ID      = "InvId"
+	QUERY_CRC         = "SignatureValue"
+	QUERY_DESCRIPTION = "Desc"
+	QUERY_LOGIN       = "MrchLogin"
+	ROBOKASSA_HOST    = "auth.robokassa.ru"
+	ROBOKASSA_PATH    = "Merchant/Index.aspx"
+	PROTO             = "https"
+	DELIMETER         = ":"
+)
+
+var (
+	ErrIncorrectValue = errors.New("incorrect value")
+)
+
+type Client struct {
+	login          string
+	firstPassword  string
+	secondPassword string
+}
+
+func (client *Client) Url(invoice, value int, description string) (string, error) {
+	return buildRedirectUrl(client.login, client.firstPassword, invoice, value, description)
+}
+
+func (client *Client) Check(r *http.Request) bool {
+	return verifyRequest(client.secondPassword, r)
+}
+
+func New(login, password1, password2 string) *Client {
+	return &Client{login, password1, password2}
+}
+
+// join values with delimeter and return hex of md5
+func CRC(v ...interface{}) string {
+	s := make([]string, len(v))
+	for key, value := range v {
+		s[key] = fmt.Sprintf("%v", value)
+	}
+	h := md5.New()
+	io.WriteString(h, strings.Join(s, DELIMETER))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func buildRedirectUrl(login, password string, invoice, value int, description string) (string, error) {
+	if value < 0 {
+		return "", ErrIncorrectValue
+	}
+
+	q := url.URL{}
+	q.Host = ROBOKASSA_HOST
+	q.Scheme = PROTO
+	q.Path = ROBOKASSA_PATH
+
+	params := url.Values{}
+	params.Add(QUERY_LOGIN, login)
+	params.Add(QUERY_OUT_SUMM, strconv.Itoa(value))
+	params.Add(QUERY_INV_ID, strconv.Itoa(invoice))
+	params.Add(QUERY_DESCRIPTION, description)
+	params.Add(QUERY_CRC, CRC(login, value, invoice, password))
+
+	q.RawQuery = params.Encode()
+
+	return q.String(), nil
+}
+
+func verifyResult(password string, invoice, value int, crc string) bool {
+	return strings.ToUpper(crc) == strings.ToUpper(CRC(value, invoice, password))
+}
+
+func verifyRequest(password string, r *http.Request) bool {
+	q := r.URL.Query()
+	value, err := strconv.Atoi(q.Get(QUERY_OUT_SUMM))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	invoice, err := strconv.Atoi(q.Get(QUERY_INV_ID))
+	if err != nil {
+		return false
+	}
+	crc := q.Get(QUERY_CRC)
+	return verifyResult(password, invoice, value, crc)
+}
---
+++ gorobokassa_test.go
@@ -0,0 +1,60 @@
+package gorobokassa
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"log"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestUrlGeneration(t *testing.T) {
+	Convey("Url", t, func() {
+		in, err := buildRedirectUrl("lel", "lel", 11, 500, "KEK")
+		out := "https://auth.robokassa.ru/Merchant/Index.aspx?Desc=KEK&InvId=11&MrchLogin=lel&OutSum=500&SignatureValue=e8c1c7bcacfa991b8612f2759804abd9"
+		So(err, ShouldBeNil)
+		So(in, ShouldEqual, out)
+		Convey("Less than zero", func() {
+			_, err := buildRedirectUrl("d", "s", 11, -200, "test")
+			So(err, ShouldNotBeNil)
+		})
+	})
+	Convey("Result", t, func() {
+		request := &http.Request{}
+		q := url.URL{}
+		params := url.Values{}
+		params.Add("OutSum", "1200")
+		params.Add("InvId", "666")
+		params.Add("SignatureValue", "3a3869287aaa475dda04d93280705839")
+		q.RawQuery = params.Encode()
+		request.URL = &q
+		log.Println(q.String())
+		So(verifyResult("password", 666, 1200, "3a3869287aaa475dda04d93280705839"), ShouldBeTrue)
+		So(verifyResult("password", 666, 1200, "3a3869287aaa475dda04e93280705839"), ShouldBeFalse)
+		So(verifyRequest("password", request), ShouldBeTrue)
+		So(verifyRequest("test", request), ShouldBeFalse)
+	})
+
+	Convey("Client", t, func() {
+
+		c := New("login", "pwd1", "password")
+		Convey("Url", func() {
+			in, err := c.Url(110, 2000, "description")
+			out := "https://auth.robokassa.ru/Merchant/Index.aspx?Desc=description&InvId=110&MrchLogin=login&OutSum=2000&SignatureValue=1364f38f54e76a0affe62974bfdbde85"
+			So(err, ShouldBeNil)
+			So(in, ShouldEqual, out)
+		})
+		Convey("Check", func() {
+			request := &http.Request{}
+			q := url.URL{}
+			params := url.Values{}
+			params.Add("OutSum", "1200")
+			params.Add("InvId", "666")
+			params.Add("SignatureValue", "3a3869287aaa475dda04d93280705839")
+			q.RawQuery = params.Encode()
+			request.URL = &q
+			So(c.Check(request), ShouldBeTrue)
+		})
+
+	})
+}
